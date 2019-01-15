960d615b4ac2e4f0e98ed094eb9b224325a4c3a8
Wang Xiaolin <wx672ster@gmail.com>
2019-01-15 16:27:25 +0800

    complete of 1.5.2

---
+++ ch1/a2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("%f\n",111 * 111);
+
+  return 0;
+}
---
+++ ch1/a3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+  printf("%f\n", sqrt(-10));
+
+  return 0;
+}

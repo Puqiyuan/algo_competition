821373842506a7eedaa65a499260d5728b9c4b0b
Qiyuan Pu <pqy7172@gmail.com>
2019-01-22 17:12:38 +0800

    complete of 2-3 exercise

--- ch2/exercise/2-3.c
+++ ch2/exercise/2-3.c
@@ -2,6 +2,28 @@
 
 int main(int argc, char *argv[])
 {
+	int i, n, j;
+	scanf("%d", &n);
+
+	int tot = 2 * n - 1, delta = 0, cur = tot;
+	for (i = 0; i <= n - 1; i++)
+		{
+			j = 0;
+			while(j < i)
+				{
+					printf(" ");
+					j++;
+				}
+			
+			j = 0;
+			while(j < cur)
+				{
+					printf("#");
+					j++;
+				}
+			printf("\n");
+			cur -= 2;
+		}
 	
 	return 0;
 }
